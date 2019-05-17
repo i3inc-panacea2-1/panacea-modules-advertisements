24ab0600b4fc44b971a37868e76be1df368f553a
Portno <j.remoundos@dotbydot.gr>
2019-05-17 15:35:25 +0300

    fixes

--- src/Panacea.Modules.Advertisements/AdvertisementsPlugin.cs
+++ src/Panacea.Modules.Advertisements/AdvertisementsPlugin.cs
@@ -7,7 +7,6 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace Panacea.Modules.Advertisements
@@ -17,42 +16,11 @@ namespace Panacea.Modules.Advertisements
         PanaceaServices _core;
         public AdvertisementsPlugin(PanaceaServices core)
         {
-            CacheImage.ImageUrlChanged += CacheImage_ImageUrlChanged;
+            
             _core = core;
         }
 
-        private void CacheImage_ImageUrlChanged(object sender, string e)
-        {
-            var image = sender as CacheImage;
-            if (!image.IsLoaded)
-            {
-                image.Loaded += Image_Loaded;
-                return;
-            }
-            var task = SetImage(image, e);
-
-        }
-
-        private void Image_Loaded(object sender, RoutedEventArgs e)
-        {
-            var image = sender as CacheImage;
-            image.Loaded -= Image_Loaded;
-            var task = SetImage(image, image.ImageUrl);
-        }
-
-        async Task SetImage(CacheImage image, string url)
-        {
-            var download = await _core.HttpClient.DownloadDataAsync(url);
-            var img2 = new BitmapImage();
-            img2.BeginInit();
-            img2.CreateOptions |= BitmapCreateOptions.IgnoreColorProfile;
-            img2.CacheOption = BitmapCacheOption.OnLoad;
-            img2.StreamSource = new MemoryStream(download);
-
-            img2.EndInit();
-            img2.Freeze();
-            image.Source = img2;
-        }
+        
 
         public Task BeginInit()
         {
