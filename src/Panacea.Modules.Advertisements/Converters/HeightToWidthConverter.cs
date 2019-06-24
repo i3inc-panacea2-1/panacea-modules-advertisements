f7d4769a5f05461e5b9c86428cf5f6ca6f3c5e38
Portno <j.remoundos@dotbydot.gr>
2019-06-24 12:44:47 +0300

    nuget update + text sizes

---
+++ src/Panacea.Modules.Advertisements/Converters/HeightToWidthConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Panacea.Modules.Advertisments.Converters
+{
+    class HeightToWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value * 5.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
