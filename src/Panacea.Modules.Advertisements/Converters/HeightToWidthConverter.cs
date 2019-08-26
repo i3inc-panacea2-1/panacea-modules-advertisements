d9afffe7fa86e7ab71ece2ab828b7da0bd6cb0fc
Portno <j.remoundos@dotbydot.gr>
2019-08-26 14:45:26 +0300

    fixed plugin name issues

--- src/Panacea.Modules.Advertisements/Converters/HeightToWidthConverter.cs
+++ src/Panacea.Modules.Advertisements/Converters/HeightToWidthConverter.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace Panacea.Modules.Advertisments.Converters
+namespace Panacea.Modules.Advertisements.Converters
 {
     class HeightToWidthConverter : IValueConverter
     {
